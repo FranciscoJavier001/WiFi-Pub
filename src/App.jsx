20b98f3426df45a504acda72d0e6e0ad20d88c52
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2025-06-29 16:17:40 -0600

    WebApp

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-white text-black font-sans flex flex-col items-center justify-center p-8">
+      <header className="text-center mb-20">
+        <h1 className="text-5xl font-bold mb-4">🛜 Wi-Fi 7 🛜<br />Una realidad </h1>
+        <p className="text-xl">Ya no hay margen para errores.</p>
+        <p className="text-lg mt-4 text-gray-600">Bienvenido a una red diseñada por ingenieros. <br /><br />Estable, rápida y sin pretextos.</p>
+      </header>
+      <section className="grid grid-cols-1 md:grid-cols-3 gap-8 text-center mb-20">
+        <div>
+          <h2 className="text-2xl font-semibold">✅ Estabilidad Garantizada ✅</h2>
+        </div>
+        <div>
+          <h2 className="text-2xl font-semibold">🚀 Super Velocidad 🚀</h2>
+        </div>
+        <div>
+          <h2 className="text-2xl font-semibold">👨‍💻 Soporte Profesional 👨‍💻</h2>
+        </div>
+      </section>
+      <div className="text-center text-2xl font-medium mb-16">
+        “Diseño de Red Profesional para Areas Industriales.”
+      </div>
+      <footer className="mt-20 text-sm text-gray-500"> <br />
+        Conectividad sin interrupciones.
+      </footer>
+    </div>
+  )
+}
+
+export default App
